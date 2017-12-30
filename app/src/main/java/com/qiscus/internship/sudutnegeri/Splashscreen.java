59e386d37d9cc715cc1bdc9f8fa60b1dc7927fd6
vizyan <vizyan.1@gmail.com>
2017-12-30 17:18:51 +0700

    First Commit

---
+++ app/src/main/java/com/qiscus/internship/sudutnegeri/Splashscreen.java
@@ -0,0 +1,39 @@
+package com.qiscus.internship.sudutnegeri;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Splashscreen extends AppCompatActivity {
+    private static int splashInterval = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(Splashscreen.this, MainActivity.class);
+                startActivity(i);
+                this.finish();
+            }
+
+            private void finish() {
+                // TODO Auto-generated method stub
+
+            }
+        }, splashInterval);
+
+    }
+}
