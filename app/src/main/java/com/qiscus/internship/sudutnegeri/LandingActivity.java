3198661de0e713a2de775c29e39baa04b5f109b5
vizyan <vizyan.1@gmail.com>
2018-01-02 11:42:08 +0700

    2 Januari 2018 - Login

--- app/src/main/java/com/qiscus/internship/sudutnegeri/LandingActivity.java
+++ app/src/main/java/com/qiscus/internship/sudutnegeri/LandingActivity.java
@@ -1,7 +1,11 @@
 package com.qiscus.internship.sudutnegeri;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.Button;
 
 public class LandingActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class LandingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+        Button btnRegister = findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent login = new Intent(LandingActivity.this, LoginActivity.class);
+                startActivity(login);
+                finish();
+            }
+        });
     }
 }
