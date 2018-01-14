02e43fb4acffa72b3f0253bd7ca04d981f1e767d
Vizyan <vizyan.1@gmail.com>
2018-01-14 14:22:52 +0700

    Change name

---
+++ app/src/main/java/com/qiscus/internship/sudutnegeri/ui/Admin/FragmentVerifyProject.java
@@ -0,0 +1,30 @@
+package com.qiscus.internship.sudutnegeri.ui.Admin;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.qiscus.internship.sudutnegeri.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentVerifyProject extends Fragment {
+
+
+    public FragmentVerifyProject() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment_verify_project, container, false);
+    }
+
+}
