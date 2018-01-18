4dbb440e9f0695343b3c801a686b8cc3515cbe7a
Vizyan <vizyan.1@gmail.com>
2018-01-18 15:57:36 +0700

    Add Popup Verif Project

--- app/src/main/java/com/qiscus/internship/sudutnegeri/ui/project/ProjectActivity.java
+++ app/src/main/java/com/qiscus/internship/sudutnegeri/ui/project/ProjectActivity.java
@@ -1,26 +1,35 @@
 package com.qiscus.internship.sudutnegeri.ui.project;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.qiscus.internship.sudutnegeri.R;
 import com.qiscus.internship.sudutnegeri.data.model.DataProject;
 import com.qiscus.internship.sudutnegeri.data.model.DataUser;
+import com.qiscus.internship.sudutnegeri.ui.admin.AdminActivity;
+import com.qiscus.internship.sudutnegeri.ui.user.UserActivity;
 import com.qiscus.internship.sudutnegeri.util.Constant;
 import com.squareup.picasso.Picasso;
 
@@ -30,15 +39,14 @@ public class ProjectActivity extends AppCompatActivity implements ProjectView {
 
     private ProjectPresenter projectPresenter;
     private DataProject dataProject;
-    private DataUser dataUser;
-    Button btnProjectDonate;
+    Button btnProjectDonate, btnPopupSNext;
     FloatingActionButton fabProjectChat;
     ImageView ivProjectPhoto;
     int funds = 0;
     JustifyTextView jtProjectInformation;
     ProgressBar pbProjectProgress;
     String param;
-    TextView tvProjectName, tvProjectLocation, tvProjectTarget, tvProjectCreator;
+    TextView tvProjectName, tvProjectLocation, tvProjectTarget, tvProjectCreator, tvPopupFMsg, tvPopupFType, tvPopupSMsg, tvPopupSType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -103,6 +111,58 @@ public class ProjectActivity extends AppCompatActivity implements ProjectView {
         });
     }
 
+    private void popupWindow(String messsage, String param) {
+        try {
+            LayoutInflater inflater = (LayoutInflater) ProjectActivity.this
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            if (messsage.equals("success")){
+                View layout = inflater.inflate(R.layout.layout_popup_success, null);
+                final PopupWindow pw = new PopupWindow(layout, RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT, true);
+                pw.setOutsideTouchable(false);
+                pw.showAtLocation(layout, Gravity.CENTER, 0, 0);
+                btnPopupSNext = layout.findViewById(R.id.btnPopupSNext);
+                tvPopupSMsg = layout.findViewById(R.id.tvPopupSMsg);
+                tvPopupSType = layout.findViewById(R.id.tvPopupSType);
+
+                btnPopupSNext.setVisibility(View.GONE);
+
+                if(param.equalsIgnoreCase("admin")){
+                    tvPopupSType.setText("Verifikasi berhasil");
+                    tvPopupSMsg.setText("Project " + dataProject.getNameProject());
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            // TODO Auto-generated method stub
+                            Intent admin = new Intent(ProjectActivity.this, AdminActivity.class);
+                            startActivity(admin);
+                            ProjectActivity.this.finish();
+                        }
+
+                        private void finish() {
+                            // TODO Auto-generated method stub
+
+                        }
+                    }, 1000);
+                } else {
+                    tvPopupSType.setText("Selamat");
+                    tvPopupSMsg.setText("Profil anda berhasil diperbarui");
+                }
+            } else {
+                View layout = inflater.inflate(R.layout.layout_popup_failed, null);
+                final PopupWindow pw = new PopupWindow(layout, RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT, true);
+                pw.showAtLocation(layout, Gravity.CENTER, 0, 0);
+                tvPopupFMsg = layout.findViewById(R.id.tvPopupFMsg);
+                tvPopupFType = layout.findViewById(R.id.tvPopupFType);
+                tvPopupFMsg.setText(messsage);
+                tvPopupFType.setText("Gagal merubah profil");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     public void successProjectById(DataProject dataProject) {
         tvProjectName.setText(dataProject.getNameProject());
@@ -122,6 +182,7 @@ public class ProjectActivity extends AppCompatActivity implements ProjectView {
     @Override
     public void successPutProject(DataProject dataProject) {
         btnProjectDonate.setVisibility(View.INVISIBLE);
+        popupWindow("success", param);
     }
 
     @Override
